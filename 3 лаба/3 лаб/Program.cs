1efbfd34e12fc306563334883501553844359b23
Jungle-03 <77698511+Jungle-03@users.noreply.github.com>
2021-12-30 12:14:36 +0300

    Add files via upload

---
+++ 3 лаба/3 лаб/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+
+namespace _3_лаб
+{
+
+    public class Car
+    {
+        //начнем объявления свойств
+
+        private string marka;
+        public string Marka
+        {
+            get { return marka; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    marka = value;
+                else
+                    Console.WriteLine("Неккоретные данные");
+            }
+        }
+
+        private string model;
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+
+        }
+
+        private int dateOfmade;
+        public int DateOfmade
+        {
+            get { return dateOfmade; }
+            set { dateOfmade = value; }
+        }
+        private string color;
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        private int price;
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        private string number;
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public static int Counter { get; private set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"Марка\t{marka} ");
+            Console.WriteLine($"Модель\t{model} ");
+            Console.WriteLine($"Номер\t{number} ");
+            Console.WriteLine($"Дата производства\t{dateOfmade} ");
+            Console.WriteLine($"Цвет\t{color} ");
+            Console.WriteLine($"Цена\t{price} ");
+
+        }
+
+        public Car(string marka, string model, int dateOfmade, string color, int price, string number) 
+        {
+
+            Marka = marka;
+            Model = model;
+            DateOfmade = dateOfmade;
+            Color = color;
+            Price = price;
+            Number = number;
+
+            Counter++;
+        }
+        //делаю 2й конструктор
+        public Car()//конструктор без параметров
+        {
+            
+        }
+        // 3й (параметры по умолчанию)
+        public Car(string marka = "Kakein", string model="1234",int dateOfmade = 2010,string color ="Pink", int price = 4000,string number ="JOJO DIO-5",string kyzov ="pupe") 
+        {
+
+
+            Marka = marka;
+            Model = model;
+            DateOfmade = dateOfmade;
+            Color = color;
+            Price = price;
+            Number = number;
+
+
+            Counter++;
+         }
+        // саттический конструктор
+        static Car()
+        {
+           
+        }
+
+        
+     
+    }
+
+
+
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+
+
+                Car[] cars = new Car[3];
+
+
+                Console.WriteLine("Информация о машинах", "\n");
+
+                cars[0] = new Car("Merin", "228X", 2020, "White", 10000, "1000 AV-7");
+
+
+
+                cars[1] = new Car("BMW", "QWE", 2021, "Black", 12000, "1032 AG-6");
+
+
+                cars[2] = new Car("Merin", "ZXC", 2021, "Black", 993000, "1000 ZXC-7");
+
+
+
+                //Вывод всех машин
+
+                for (int i = 0; i < cars.Length; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+
+                    cars[i].Print();
+
+                    Console.WriteLine('\n');
+
+                }
+
+                Console.WriteLine($"Колличество машин: {Car.Counter}", "\n");
+
+
+
+                // вывод определеннх машин
+
+
+
+                string zzz;
+                Console.WriteLine("Напишите Название марки");
+                zzz = Console.ReadLine();
+                Console.WriteLine("\n");
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                for (int i = 0; i < cars.Length; i++)
+                {
+                    if (cars[i].Marka == zzz)
+                        cars[i].Print();
+                }
+
+                Console.WriteLine("\n");
+
+                Console.WriteLine("Напишите дату изготовления с которой начтется наш список", "\n");
+
+                int DDData;
+                DDData = int.Parse(Console.ReadLine());
+
+
+                for (int i = 0; i < cars.Length; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    if (cars[i].DateOfmade >= DDData)
+
+                        cars[i].Print();
+                    Console.WriteLine('\n');
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+
+
+
+
+            }
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+        
---
+++ 4 лаба/4 лаба/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _4_лаба
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            //создание 1-го листа
+            List<string> socialNetworks = new List<string>();
+
+            socialNetworks.Add("VK");
+            socialNetworks.Add("Instagram");
+            socialNetworks.Add("Telegram");
+
+
+
+
+            Console.WriteLine("Список социальных сетей:\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            //вывод
+            foreach (string SOCIALnETWORKS in socialNetworks)
+            {
+
+                Console.WriteLine(SOCIALnETWORKS);
+
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+            //создание 2-го листа
+            List<string> dotaHeros = new List<string>();
+
+
+            dotaHeros.Add("Marci");
+            dotaHeros.Add("Windranger");
+            dotaHeros.Add("Skywrath Mage");
+
+
+            Console.WriteLine("\nМикро список героев в доте:\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            //вывод
+            foreach (string Pers in dotaHeros)
+            {
+
+                Console.WriteLine(Pers);
+
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+            //проверка на равенство колличества элементов
+            if (socialNetworks.Count == dotaHeros.Count)
+            {
+                Console.WriteLine("\nКолличество элементов в списках равны друг другу");
+
+            }
+            else
+            {
+                Console.WriteLine("\nКолличество элементов в списках не равны друг другу");
+            }
+
+            //проверка на равенство самих элементов
+
+            if (socialNetworks == dotaHeros)
+            {
+                Console.WriteLine("\nЭлементы 2-х списков одинаковые ");
+            }
+            else
+            {
+                Console.WriteLine("\nЭлементы 2-х списков не одинаковые ");
+            }
+
+            //да/нет
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            bool zxc = dotaHeros.SequenceEqual(socialNetworks);
+            Console.WriteLine($"\nДота и соц.цети\t{zxc}\n");
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+
+
+            //удаление элемента из начала списка
+
+            socialNetworks.RemoveAt(0);
+
+            Console.WriteLine("\nСписок соц.сетей с удаленным 1-м элементом:\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            //вывод
+            foreach (string SOCIALnETWORKS in socialNetworks)
+            {
+
+                Console.WriteLine(SOCIALnETWORKS);
+
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+            //получаем(выделяем последний элемент наших листов
+
+            Console.WriteLine("\nПервый элемент листа socialNetworks\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine(socialNetworks[0]);
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\nПервый элемент листа dotaHeros\n");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+
+            Console.WriteLine(dotaHeros[0]);
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+            //слияние 2-х листов
+
+            socialNetworks.AddRange(dotaHeros);
+
+
+            Console.WriteLine("\n\nОбъеденение листов");
+
+
+            foreach (string bought in socialNetworks)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine('\n');
+                Console.Write(bought);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("\n\n\n\n");
+
+            List<int> num = new List<int> { 1000, 993, 986, 979, 972, 965 };
+            string str = "Лобби ?";
+
+
+            DateTime.Now.Print();
+
+
+
+            num.Diff();
+            num.Sum();
+            num.Elements();
+            str.Upper();
+
+                 
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+
+        }
+    }
+
+    class Owner
+    {
+        public string name;
+        public string organization;
+        public int id;
+    }
+
+    public class MYList<T> : List<T>
+    {
+        private Owner student = new Owner { name = "Jungle", organization = "BSTU", id = 9 };
+
+        public static MYList<T> operator +(MYList<T> a, T b)
+        {
+            MYList<T> result = new MYList<T>();
+            result = a;
+            result.Add(b);
+            return result;
+        }
+        public static MYList<T> operator --(MYList<T> a)
+        {
+            MYList<T> result = new MYList<T>();
+            result = a;
+            result.RemoveAt(result.Count() - 1);
+            return result;
+        }
+        public static bool operator !=(MYList<T> a, MYList<T> b)
+        {
+            if (a.Equals(b))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static bool operator ==(MYList<T> a, MYList<T> b)
+        {
+            if (a.Equals(b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+    }
+
+    static class StatisticOperation
+    {
+        public static void Print(this DateTime date)
+        {
+            Console.WriteLine(date);
+        }
+
+
+        public static int Elements(this List<int> num)
+        {
+            int counter = 0;
+            for (int i = 0; i < num.Count; i++)
+            {
+                counter++;
+            }
+            Console.WriteLine($"Элементы:\t {counter}");
+            return counter;
+        }
+        public static int Diff(this List<int> num)
+        {
+            int max = num.Max();
+            int min = num.Min();
+            int diff = max - min;
+            Console.WriteLine($"Разница между макс и мин\t {diff}");
+            return diff;
+        }
+        public static int Sum(this List<int> num)
+        {
+            int sum = 0;
+            for (int i = 0; i < num.Count; i++)
+            {
+                sum += num[i];
+            }
+            Console.WriteLine($"Сумма\t {sum}");
+            return sum;
+        }
+
+
+        public static int Upper(this string s)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsUpper(s[i]))
+                    count++;
+            }
+            Console.WriteLine($"Слов с заглавной буквы:\t {count}");
+            return count;
+
+        }
+
+       
+    }
+}
+    
+        
