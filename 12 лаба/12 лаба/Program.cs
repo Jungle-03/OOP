888347665549f40105f14c37d0ee64d3c0fc3450
Jungle-03 <77698511+Jungle-03@users.noreply.github.com>
2021-12-30 12:20:18 +0300

    Add files via upload

---
+++ 12 лаба/12 лаба/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12_лаба
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
